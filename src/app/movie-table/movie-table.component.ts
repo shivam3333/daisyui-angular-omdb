d7b6b6fd04bda293ceb5153468f3b5f88e123267
shivam3333 <chauhan.shivam190@gmail.com>
2023-06-10 14:45:48 +0530

    Added pagination in table list

--- src/app/movie-table/movie-table.component.ts
+++ src/app/movie-table/movie-table.component.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { PaginationInstance } from 'ngx-pagination';
 import { HttpClient } from '@angular/common/http';
-const APIKEY = "638472b7";
+const APIKEY = '638472b7';
 
 @Component({
   selector: 'app-movie-table',
   templateUrl: './movie-table.component.html',
-  styleUrls: ['./movie-table.component.css']
+  styleUrls: ['./movie-table.component.css'],
 })
-
-
 export class MovieTableComponent implements OnInit {
-  
   movies: any[] = [];
   page: number = 1;
-  constructor(private http: HttpClient) { }
+  config: PaginationInstance = {
+    id: 'omdb',
+    itemsPerPage: 10,
+    currentPage: 1,
+    totalItems: 0
+  };
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.fetchMovies();
   }
 
   fetchMovies() {
-    this.http.get(`https://www.omdbapi.com/?s=movie&apikey=${APIKEY}`)
+    this.http
+      .get(
+        `https://www.omdbapi.com/?s=movie&apikey=${APIKEY}&page=${this.config.currentPage}`
+      )
       .subscribe((response: any) => {
         this.movies = response.Search;
+        this.config.totalItems = response.totalResults;
         this.sortByReleaseDate();
-        console.log('response.Search', response.Search);
+        console.log('response.Search', response);
       });
   }
 
@@ -36,4 +44,8 @@ export class MovieTableComponent implements OnInit {
     });
   }
 
+  onPageChange(page: number) {
+    this.config.currentPage = page;
+    this.fetchMovies();
+  }
 }
